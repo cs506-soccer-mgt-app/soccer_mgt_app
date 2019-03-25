fd033ac9096f7960e53a238ba572a6278a3ba023
Ryan Gontarek <rgontarek@wisc.edu>
2019-03-25 17:22:23 -0500

    finished api calls for iteration one

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -10,21 +10,17 @@ export class TeamService {
   constructor(public http: HttpClient) {
 
     // // Get All Teams
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/team').subscribe((data) => {
+    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams').subscribe((data) => {
     //   console.log(data);
     // });
-
 
     // // Get Team by Team Id
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/team/teamid/2').subscribe((data) => {
+    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/2').subscribe((data) => {
     //   console.log(data);
     // });
 
-    // /production/team/2
-
-
     // // Post Team
-    // this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/team', 
+    // this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams', 
     // {"name":"Wolf",
     //   "primary_color":"Red", 
     //   "session_id": "4",
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,22 +10,22 @@ export class UserService {
   constructor(public http: HttpClient) {
 
     // // Get All Users
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/user').subscribe((data) => {
+    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users').subscribe((data) => {
+    //   console.log(data);
+    // });
+    
+    // // Get User by Id
+    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users/2').subscribe((data) => {
     //   console.log(data);
     // });
 
-    // // Get User by Id
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/user/userid/2').subscribe((data) => {
-    //   console.log(data);
-    // });
-
-    // // Get User by Id
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/test/user/1').subscribe((data) => {
+    // // Get All Teams for User by Id
+    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users/2/teams').subscribe((data) => {
     //   console.log(data);
     // });
 
     // // Post User
-    // this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/user', 
+    // this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users', 
     // {"firstname":"Qwer",
     //   "lastname":"Qwer", 
     //   "password":"qwerqwer", 

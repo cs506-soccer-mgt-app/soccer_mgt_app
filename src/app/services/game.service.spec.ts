430785e5d6a88079bbe210faa6f8b5eb3406c9a7
Ryan Gontarek <rgontarek@wisc.edu>
2019-03-25 19:29:16 -0500

    added to the spec files for game service

--- src/app/services/game.service.spec.ts
+++ src/app/services/game.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { GameService } from './game.service';
 
 describe('GameService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GameService]
+  }));
 
   it('should be created', () => {
     const service: GameService = TestBed.get(GameService);

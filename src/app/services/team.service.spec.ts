1262fa6f3ec3d399d98eb0e52c45417bf9a84d73
Benjamin Nisler <bnisler@gmail.com>
2019-03-26 05:38:02 -0500

    include POST and PUT methods for team service; added tests

--- src/app/services/team.service.spec.ts
+++ src/app/services/team.service.spec.ts
@@ -20,15 +20,46 @@ describe('TeamService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return specific team data when passing a team_id of 1', () => {
+  it('should GET specific team data when passing a team_id of 1', () => {
     const { teamService, httpTestingCtrl } = setup();
     const mockData = { team_id: 1, name: 'GOAT', primary_color: 'Red', alternate_color: 'White', manager_id: 1, session_id: 3 };
-    teamService.getTeamDetail(mockData.team_id).subscribe(data => {
-      expect(data.data).toEqual(mockData);
-    });
+    teamService.getTeamDetail(mockData.team_id)
+        .subscribe(data => {
+          expect(data.data).toEqual(mockData);
+        });
 
-    const req = httpTestingCtrl.expectOne('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/team/1');
+    const req = httpTestingCtrl.expectOne('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/1');
     expect(req.request.method).toBe('GET');
+    req.flush({
+      data: mockData
+    });
+  });
+
+  it('should POST a new team to the team resource', () => {
+    const { teamService, httpTestingCtrl } = setup();
+    const mockData = { team_id: 999, name: '999 GOAT', primary_color: 'Blue', alternate_color: 'White', manager_id: 1, session_id: 3 };
+    teamService.createTeam(mockData.name, mockData.primary_color, mockData.session_id, mockData.alternate_color, mockData.manager_id)
+        .subscribe(data => {
+          expect(data.data).toBe(mockData);
+        });
+
+    const req = httpTestingCtrl.expectOne('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams');
+    expect(req.request.method).toBe('POST');
+    req.flush({
+      data: mockData
+    });
+  });
+
+  it('should PUT a team to the team resource with new team data', () => {
+    const { teamService, httpTestingCtrl } = setup();
+    const mockData = { team_id: 999, name: '999 GOAT 999', primary_color: 'Black', alternate_color: 'White', manager_id: 1, session_id: 3 };
+    teamService.updateTeam(mockData.team_id, mockData.name, mockData.primary_color, mockData.session_id, mockData.alternate_color, mockData.manager_id)
+        .subscribe(data => {
+          expect(data.data).toBe(mockData);
+        });
+
+    const req = httpTestingCtrl.expectOne('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/999');
+    expect(req.request.method).toBe('PUT');
     req.flush({
       data: mockData
     });
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -7,41 +7,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TeamService {
 
-  constructor(public http: HttpClient) {
-
-    // // Get All Teams
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams').subscribe((data) => {
-    //   console.log(data);
-    // });
-
-    // // Get Team by Team Id
-    // this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/2').subscribe((data) => {
-    //   console.log(data);
-    // });
-
-    // // Post Team (also known as creating a team)
-    // this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams', 
-    // {"name":"Wolf",
-    //   "primary_color":"Red", 
-    //   "session_id": "4",
-    //   "alternate_color":"Purple", 
-    //   "manager_id":"1"}).subscribe((data) => {
-    //   console.log(data)
-    // });
-
-    // // Put Team (also known as modifying an already craeted teams attributes)
-    // this.http.put('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/1', 
-    // {"name":"Wolf2.0",
-    //   "primary_color":"Red2.0", 
-    //   "session_id": "4",
-    //   "alternate_color":"Purple2.0", 
-    //   "manager_id":"1"}).subscribe((data) => {
-    //   console.log(data)
-    // });
-
-   }
+  constructor(public http: HttpClient) { }
 
    getTeamDetail(id: number) {
       return this.http.get('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/' + id);
+   }
+
+   createTeam(name: string, primary_color: string, session_id: number, alternate_color: string, manager_id: number) {
+      return this.http.post('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams', {
+          name: name,
+          primary_color: primary_color,
+          session_id: session_id,
+          alternate_color: alternate_color,
+          manager_id: manager_id
+      });
+   }
+
+   updateTeam(id: number, name: string, primary_color: string, session_id: number, alternate_color: string, manager_id: number) {
+      return this.http.put('https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/teams/' + id, {
+          name: name,
+          primary_color: primary_color,
+          session_id: session_id,
+          alternate_color: alternate_color,
+          manager_id: manager_id
+      });
    }
 }

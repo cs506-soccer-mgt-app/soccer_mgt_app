08c87938297cb68e7bdf0604a96f2907b9b9e7b2
Matthew <matthew.wright@wisc.edu>
2019-03-29 17:10:02 -0500

    Fixed testing errors for the Edit Game and Game Details pages.

--- src/app/game/game-details/game-details.page.spec.ts
+++ src/app/game/game-details/game-details.page.spec.ts
@@ -2,6 +2,9 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameDetailsPage } from './game-details.page';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/common';
 
 describe('GameDetailsPage', () => {
   let component: GameDetailsPage;
@@ -11,6 +14,11 @@ describe('GameDetailsPage', () => {
     TestBed.configureTestingModule({
       declarations: [ GameDetailsPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterModule.forRoot([]), HttpClientTestingModule],
+      providers: [
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
+        { provide: APP_BASE_HREF, useValue: '/game-details' }
+      ]
     })
     .compileComponents();
   }));

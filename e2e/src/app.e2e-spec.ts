99477185e5739231f8cb56919f860c37a898c779
JWFunk <JWFunk@outlook.com>
2019-04-11 17:59:31 -0500

    Added test for editing game e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -14,54 +14,54 @@ describe('new App', () => {
     beforeEach(() => {
       browser.get('/home');
     });
-    // it('Home screen appears', () => {
-    //   app.getPageOneTitleText().then(title => {
-    //     expect(title).toEqual('Home');
-    //   });
-    //
-    // });
-    // it('allows navigation to Team 1(GOAT Team)', () => {
-    //   app.navigateTo('team-details/1');
-    //   browser.wait(ExpectedConditions.visibilityOf(element(by.id('title'))), 3000);
-    // });
-    // it('displays GOAT Team info', () => {
-    //   app.navigateTo('team-details/1');
-    //   expect(element(by.id('title')).getText()).toEqual('GOAT');
-    // });
-    // //Problems with rendering the button if it's off the AppPage
-    // //set to a team with no games as a workaround currently
-    // //figure out how to scroll down
-    // it('allows navigation to Add Game Team 2', ()=> {
-    //   app.navigateTo('team-details/2');
-    //   var el = element(by.id('Add-Game'));
-    //   var e = element(by.id('add-game-title'));
-    //   el.click();
-    //   browser.wait(EC.presenceOf(e), 10000);
-    // });
-    // it('allows navigation to Create Team Page', () => {
-    //   app.navigateTo('create-team');
-    //   expect(element(by.id('title')).getText()).toEqual('Create Team');
-    // });
-    // it('allows navigation to game-details 1', () => {
-    //   app.navigateTo('team-details/1');
-    //   app.navigateTo('game-details/1');
-    //   expect(element(by.id('title')).getText()).toEqual('Game Details');
-    // });
-    // it('allows navigation to edit-game 1', () => {
-    //   app.navigateTo('team-details/1');
-    //   app.navigateTo('game-details/1');
-    //   element(by.id('Edit-Game')).click();
-    //   var e = element(by.id('edit-title'));
-    //   browser.wait(EC.presenceOf(e), 10000);
-    //   expect(element(by.id('edit-title')).getText()).toEqual('Edit Game');
-    // });
-    // it('allows navigation to edit-team 2', () => {
-    //   app.navigateTo('team-details/2');
-    //   element(by.id('Edit-Team')).click();
-    //   var e = element(by.id('edit-title'));
-    //   browser.wait(EC.presenceOf(e), 10000);
-    //   expect(element(by.id('edit-title')).getText()).toEqual('Edit Wolf');
-    // });
+    it('Home screen appears', () => {
+      app.getPageOneTitleText().then(title => {
+        expect(title).toEqual('Home');
+      });
+
+    });
+    it('allows navigation to Team 1(GOAT Team)', () => {
+      app.navigateTo('team-details/1');
+      browser.wait(ExpectedConditions.visibilityOf(element(by.id('title'))), 3000);
+    });
+    it('displays GOAT Team info', () => {
+      app.navigateTo('team-details/1');
+      expect(element(by.id('title')).getText()).toEqual('GOAT');
+    });
+    //Problems with rendering the button if it's off the AppPage
+    //set to a team with no games as a workaround currently
+    //figure out how to scroll down
+    it('allows navigation to Add Game Team 2', ()=> {
+      app.navigateTo('team-details/2');
+      var el = element(by.id('Add-Game'));
+      var e = element(by.id('add-game-title'));
+      el.click();
+      browser.wait(EC.presenceOf(e), 10000);
+    });
+    it('allows navigation to Create Team Page', () => {
+      app.navigateTo('create-team');
+      expect(element(by.id('title')).getText()).toEqual('Create Team');
+    });
+    it('allows navigation to game-details 1', () => {
+      app.navigateTo('team-details/1');
+      app.navigateTo('game-details/1');
+      expect(element(by.id('title')).getText()).toEqual('Game Details');
+    });
+    it('allows navigation to edit-game 1', () => {
+      app.navigateTo('team-details/1');
+      app.navigateTo('game-details/1');
+      element(by.id('Edit-Game')).click();
+      var e = element(by.id('edit-title'));
+      browser.wait(EC.presenceOf(e), 10000);
+      expect(element(by.id('edit-title')).getText()).toEqual('Edit Game');
+    });
+    it('allows navigation to edit-team 2', () => {
+      app.navigateTo('team-details/2');
+      element(by.id('Edit-Team')).click();
+      var e = element(by.id('edit-title'));
+      browser.wait(EC.presenceOf(e), 10000);
+      expect(element(by.id('edit-title')).getText()).toEqual('Edit Wolf');
+    });
     it('allows editing of game 1', () => {
       app.navigateTo('team-details/1');
       app.navigateTo('game-details/1');

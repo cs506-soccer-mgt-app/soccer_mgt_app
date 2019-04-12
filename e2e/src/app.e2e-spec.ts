94c7ae06cc02b3d11a08ef18057ad30bcd30239d
JWFunk <JWFunk@outlook.com>
2019-04-12 18:52:21 -0500

    Updated and cleaned the e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,78 +1,148 @@
-import { AppPage } from './app.po';
-import { TeamDetailsPage } from '../../src/app/team/team-details/team-details.page';
-import { TeamService } from '../../../soccer_mgt_app/src/app/services/team.service';
 import { browser, by, element, ExpectedConditions, Key } from 'protractor';
-import {ActivatedRoute} from '@angular/router';
-describe('new App', () => {
-  let teamService: TeamService;
-  let route: ActivatedRoute;
-  const app = new AppPage();
-  const teams = new TeamDetailsPage(teamService, route);
-  var EC = ExpectedConditions;
-  describe('E2E tests', () => {
-    browser.driver.manage().window().maximize();
-    beforeEach(() => {
-      browser.get('/home');
-    });
-    it('Home screen appears', () => {
-      app.getPageOneTitleText().then(title => {
-        expect(title).toEqual('Home');
-      });
 
+describe('MSMA ', () => {
+  var EC = ExpectedConditions;
+  browser.driver.manage().window().maximize();
+
+  it('Login screen appears', () => {
+    browser.get('/login');
+    var loginTitle = element(by.id('Login-Title'));
+    expect(loginTitle.getText()).toEqual('MSMA - Sign in');
+  });
+
+  it('Allows user to log in', () => {
+    var inputEmail = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
+    var inputPassword = element.all(by.css('.native-input.sc-ion-input-md')).get(1);
+    var loginButton = element(by.css('.submit-btn'));
+    var userTitle = element(by.css('.user-title'));
+    inputEmail.click();
+    inputEmail.sendKeys('netal@maillink.top');
+    inputPassword.click();
+    inputPassword.sendKeys('Password_1');
+    loginButton.click();
+    browser.wait(EC.presenceOf(userTitle), 3000);
+    expect(userTitle.getText()).toEqual('Home');
+  });
+
+  describe('E2E tests', () => {
+
+    beforeEach(() => {
+      var inputEmail = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
+      var inputPassword = element.all(by.css('.native-input.sc-ion-input-md')).get(1);
+      var loginButton = element(by.css('.submit-btn'));
+      var userTitle = element(by.css('.user-title'));
+      browser.get('/login');
+      inputEmail.click();
+      inputEmail.sendKeys('netal@maillink.top');
+      inputPassword.click();
+      inputPassword.sendKeys('Password_1');
+      loginButton.click();
+      browser.wait(EC.presenceOf(userTitle), 3000);
+      expect(userTitle.getText()).toEqual('Home');
     });
-    it('allows navigation to Team 1(GOAT Team)', () => {
-      app.navigateTo('team-details/1');
-      browser.wait(ExpectedConditions.visibilityOf(element(by.id('title'))), 3000);
-    });
-    it('displays GOAT Team info', () => {
-      app.navigateTo('team-details/1');
-      expect(element(by.id('title')).getText()).toEqual('GOAT');
-    });
-    //Problems with rendering the button if it's off the AppPage
-    //set to a team with no games as a workaround currently
-    //figure out how to scroll down
-    it('allows navigation to Add Game Team 2', ()=> {
-      app.navigateTo('team-details/2');
-      var el = element(by.id('Add-Game'));
-      var e = element(by.id('add-game-title'));
-      el.click();
-      browser.wait(EC.presenceOf(e), 10000);
-    });
+    //netal@maillink.top Password_1
+
     it('allows navigation to Create Team Page', () => {
-      app.navigateTo('create-team');
-      expect(element(by.id('title')).getText()).toEqual('Create Team');
+      var createTeam = element(by.css('.create-team'));
+      var createTitle = element(by.css('.create-title'));
+      createTeam.click();
+      expect(createTitle.getText()).toEqual('Create Team');
     });
-    it('allows navigation to game-details 1', () => {
-      app.navigateTo('team-details/1');
-      app.navigateTo('game-details/1');
-      expect(element(by.id('title')).getText()).toEqual('Game Details');
+
+    it('allows navigation to Team Page', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
     });
-    it('allows navigation to edit-game 1', () => {
-      app.navigateTo('team-details/1');
-      app.navigateTo('game-details/1');
-      element(by.id('Edit-Game')).click();
-      var e = element(by.id('edit-title'));
-      browser.wait(EC.presenceOf(e), 10000);
-      expect(element(by.id('edit-title')).getText()).toEqual('Edit Game');
+
+    it('allows navigation to edit-team', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var editTeam = element(by.css('.edit-team'));
+      var editTitle = element(by.css('.edit-title'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
+      editTeam.click();
+      browser.wait(EC.presenceOf(editTitle), 3000);
+      expect(editTitle.getText()).toEqual('Edit Test');
     });
-    it('allows navigation to edit-team 2', () => {
-      app.navigateTo('team-details/2');
-      element(by.id('Edit-Team')).click();
-      var e = element(by.id('edit-title'));
-      browser.wait(EC.presenceOf(e), 10000);
-      expect(element(by.id('edit-title')).getText()).toEqual('Edit Wolf');
+    // //Problems with rendering the button if it's off the AppPage
+    // //set to a team with no games as a workaround currently
+    // //figure out how to scroll down
+    it('allows navigation to Add Game', ()=> {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var addGame = element(by.css('.add-game'));
+      var addGameTitle = element(by.css('.add-game-title'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
+      addGame.click();
+      browser.wait(EC.visibilityOf(addGameTitle), 3000);
+      expect(addGameTitle.getText()).toEqual('Add Game');
     });
-    it('allows editing of game 1', () => {
-      app.navigateTo('team-details/1');
-      app.navigateTo('game-details/1');
-      var editButton = element(by.id('Edit-Game'));
-      browser.wait(EC.presenceOf(editButton),10000);
-      element(by.id('Edit-Game')).click();
-      var editTitle = element(by.id('edit-title'));
-      browser.wait(EC.presenceOf(editTitle), 10000);
+
+    it('allows navigation to game-details', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var game = element.all(by.css('.game')).get(0);
+      var gameTitle = element(by.css('.game-title'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
+      game.click();
+      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+    });
+
+    it('allows navigation to edit-game', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var game = element.all(by.css('.game')).get(0);
+      var gameTitle = element(by.css('.game-title'));
+      var editGame = element(by.css('.edit-game'));
+      var editTitle = element(by.css('.edit-title'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
+      game.click();
+      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+      editGame.click();
+      browser.wait(EC.presenceOf(editTitle), 1000);
       expect(editTitle.getText()).toEqual('Edit Game');
+    });
+
+    it('allows editing of game', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var game = element.all(by.css('.game')).get(0);
+      var gameTitle = element(by.css('.game-title'));
+      var editGame = element(by.css('.edit-game'));
+      var editTitle = element(by.css('.edit-title'));
       var inputOpponent = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
-      browser.wait(EC.presenceOf(inputOpponent),10000);
+      var save = element(by.css('.save'));
+      var opponent = element(by.css('.opponent'));
+      browser.wait(EC.visibilityOf(team), 3000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      expect(teamTitle.getText()).toEqual('Test');
+      game.click();
+      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+      browser.wait(EC.presenceOf(editGame),3000);
+      editGame.click();
+      browser.wait(EC.presenceOf(editTitle), 3000);
+      expect(editTitle.getText()).toEqual('Edit Game');
+      browser.wait(EC.presenceOf(inputOpponent),3000);
       inputOpponent.click();
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
@@ -83,16 +153,15 @@ describe('new App', () => {
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys('The Team');
-      element(by.id('save')).click();
-      var gameTitle = element(by.id('title'));
-      browser.wait(EC.presenceOf(gameTitle),10000);
-      expect(element(by.id('title')).getText()).toEqual('Game Details');
-      expect(element(by.id('opponent')).getText()).toEqual('The Team');
-      browser.wait(EC.presenceOf(editButton),5000);
-      element(by.id('Edit-Game')).click();
-      browser.wait(EC.presenceOf(editTitle), 10000);
+      save.click();
+      browser.wait(EC.presenceOf(gameTitle),3000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+      expect(opponent.getText()).toEqual('The Team');
+      browser.wait(EC.presenceOf(editGame),3000);
+      editGame.click();
+      browser.wait(EC.presenceOf(editTitle), 3000);
       expect(editTitle.getText()).toEqual('Edit Game');
-      browser.wait(EC.presenceOf(inputOpponent),10000);
+      browser.wait(EC.presenceOf(inputOpponent),3000);
       inputOpponent.click();
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
@@ -103,11 +172,10 @@ describe('new App', () => {
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys('Team 9');
-      element(by.id('save')).click();
-      var editTitle = element(by.id('title'));
-      browser.wait(EC.presenceOf(editTitle),10000);
-      expect(element(by.id('title')).getText()).toEqual('Game Details');
-      expect(element(by.id('opponent')).getText()).toEqual('Team 9');
+      save.click();
+      browser.wait(EC.presenceOf(gameTitle),3000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+      expect(opponent.getText()).toEqual('Team 9');
     });
   });
 });

5628ae612fb0594a254123db5d91d80b595381f8
JWFunk <JWFunk@outlook.com>
2019-04-18 15:18:18 -0500

    Added e2e test for the player user

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -20,11 +20,11 @@ describe('MSTMA ', () => {
     inputPassword.click();
     inputPassword.sendKeys('Password_1');
     loginButton.click();
-    browser.wait(EC.presenceOf(userTitle), 3000);
+    browser.wait(EC.presenceOf(userTitle), 4000);
     expect(userTitle.getText()).toEqual('Home');
   });
 
-  describe('E2E tests', () => {
+  describe('E2E tests Manager', () => {
 
     beforeEach(() => {
       var inputEmail = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
@@ -37,7 +37,7 @@ describe('MSTMA ', () => {
       inputPassword.click();
       inputPassword.sendKeys('Password_1');
       loginButton.click();
-      browser.wait(EC.presenceOf(userTitle), 3000);
+      browser.wait(EC.presenceOf(userTitle), 4000);
       expect(userTitle.getText()).toEqual('Home');
     });
     //netal@maillink.top Password_1
@@ -52,9 +52,9 @@ describe('MSTMA ', () => {
     it('allows navigation to Team Page', () => {
       var team = element.all(by.css('.team')).get(0);
       var teamTitle = element(by.css('.team-title'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
     });
 
@@ -63,12 +63,12 @@ describe('MSTMA ', () => {
       var teamTitle = element(by.css('.team-title'));
       var editTeam = element(by.css('.edit-team'));
       var editTitle = element(by.css('.edit-title'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
       editTeam.click();
-      browser.wait(EC.presenceOf(editTitle), 3000);
+      browser.wait(EC.presenceOf(editTitle), 4000);
       expect(editTitle.getText()).toEqual('Edit Test');
     });
     // //Problems with rendering the button if it's off the AppPage
@@ -79,12 +79,12 @@ describe('MSTMA ', () => {
       var teamTitle = element(by.css('.team-title'));
       var addGame = element(by.css('.add-game'));
       var addGameTitle = element(by.css('.add-game-title'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
       addGame.click();
-      browser.wait(EC.visibilityOf(addGameTitle), 3000);
+      browser.wait(EC.visibilityOf(addGameTitle), 4000);
       expect(addGameTitle.getText()).toEqual('Add Game');
     });
 
@@ -93,12 +93,12 @@ describe('MSTMA ', () => {
       var teamTitle = element(by.css('.team-title'));
       var game = element.all(by.css('.game')).get(0);
       var gameTitle = element(by.css('.game-title'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
       game.click();
-      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      browser.wait(EC.visibilityOf(gameTitle), 4000);
       expect(gameTitle.getText()).toEqual('Game Details');
     });
 
@@ -109,15 +109,15 @@ describe('MSTMA ', () => {
       var gameTitle = element(by.css('.game-title'));
       var editGame = element(by.css('.edit-game'));
       var editTitle = element(by.css('.edit-title'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
       game.click();
-      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      browser.wait(EC.visibilityOf(gameTitle), 4000);
       expect(gameTitle.getText()).toEqual('Game Details');
       editGame.click();
-      browser.wait(EC.presenceOf(editTitle), 3000);
+      browser.wait(EC.presenceOf(editTitle), 4000);
       expect(editTitle.getText()).toEqual('Edit Game');
     });
 
@@ -131,18 +131,18 @@ describe('MSTMA ', () => {
       var inputOpponent = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
       var save = element(by.css('.save'));
       var opponent = element(by.css('.opponent'));
-      browser.wait(EC.visibilityOf(team), 3000);
+      browser.wait(EC.visibilityOf(team), 4000);
       team.click();
-      browser.wait(EC.visibilityOf(teamTitle), 3000);
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
       expect(teamTitle.getText()).toEqual('Test');
       game.click();
-      browser.wait(EC.visibilityOf(gameTitle), 3000);
+      browser.wait(EC.visibilityOf(gameTitle), 4000);
       expect(gameTitle.getText()).toEqual('Game Details');
-      browser.wait(EC.presenceOf(editGame),3000);
+      browser.wait(EC.presenceOf(editGame),4000);
       editGame.click();
-      browser.wait(EC.presenceOf(editTitle), 3000);
+      browser.wait(EC.presenceOf(editTitle), 4000);
       expect(editTitle.getText()).toEqual('Edit Game');
-      browser.wait(EC.presenceOf(inputOpponent),3000);
+      browser.wait(EC.presenceOf(inputOpponent),4000);
       inputOpponent.click();
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
@@ -154,14 +154,14 @@ describe('MSTMA ', () => {
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys('The Team');
       save.click();
-      browser.wait(EC.presenceOf(gameTitle),3000);
+      browser.wait(EC.presenceOf(gameTitle),4000);
       expect(gameTitle.getText()).toEqual('Game Details');
       expect(opponent.getText()).toEqual('The Team');
-      browser.wait(EC.presenceOf(editGame),3000);
+      browser.wait(EC.presenceOf(editGame),4000);
       editGame.click();
-      browser.wait(EC.presenceOf(editTitle), 3000);
+      browser.wait(EC.presenceOf(editTitle), 4000);
       expect(editTitle.getText()).toEqual('Edit Game');
-      browser.wait(EC.presenceOf(inputOpponent),3000);
+      browser.wait(EC.presenceOf(inputOpponent),4000);
       inputOpponent.click();
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys(Key.BACK_SPACE);
@@ -173,9 +173,101 @@ describe('MSTMA ', () => {
       inputOpponent.sendKeys(Key.BACK_SPACE);
       inputOpponent.sendKeys('Team 9');
       save.click();
-      browser.wait(EC.presenceOf(gameTitle),3000);
+      browser.wait(EC.presenceOf(gameTitle),4000);
       expect(gameTitle.getText()).toEqual('Game Details');
       expect(opponent.getText()).toEqual('Team 9');
+    });
+
+
+  });
+
+  describe('E2E tests Player', () => {
+    beforeEach(() => {
+      var inputEmail = element.all(by.css('.native-input.sc-ion-input-md')).get(0);
+      var inputPassword = element.all(by.css('.native-input.sc-ion-input-md')).get(1);
+      var loginButton = element(by.css('.submit-btn'));
+      var userTitle = element(by.css('.user-title'));
+      browser.get('/login');
+      inputEmail.click();
+      inputEmail.sendKeys('hadevik@hotelnextmail.com');
+      inputPassword.click();
+      inputPassword.sendKeys('Password_1');
+      loginButton.click();
+      browser.wait(EC.presenceOf(userTitle), 4000);
+      expect(userTitle.getText()).toEqual('Home');
+    });
+
+    //hadevik@hotelnextmail.com Password_1
+    it('Allows navigation to Home Page', () => {
+      var userTitle = element(by.css('.user-title'));
+      browser.wait(EC.presenceOf(userTitle), 4000);
+      expect(userTitle.getText()).toEqual('Home');
+    });
+
+    it('allows navigation to Create Team Page', () => {
+      var createTeam = element(by.css('.create-team'));
+      var createTitle = element(by.css('.create-title'));
+      createTeam.click();
+      expect(createTitle.getText()).toEqual('Create Team');
+    });
+
+    it('allows navigation to Create Team Page', () => {
+      var createTeam = element(by.css('.create-team'));
+      var createTitle = element(by.css('.create-title'));
+      createTeam.click();
+      expect(createTitle.getText()).toEqual('Create Team');
+    });
+
+    it('Edit team button is not visible', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var editTeam = element(by.css('.edit-team'));
+      browser.wait(EC.visibilityOf(team), 4000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
+      expect(teamTitle.getText()).toEqual('Test');
+      expect(EC.invisibilityOf(editTeam));
+    });
+
+    it('Add game button is not visible', ()=> {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var addGame = element(by.css('.add-game'));
+      browser.wait(EC.visibilityOf(team), 4000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
+      expect(teamTitle.getText()).toEqual('Test');
+      expect(EC.invisibilityOf(addGame));
+    });
+
+    it('allows navigation to game-details', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var game = element.all(by.css('.game')).get(0);
+      var gameTitle = element(by.css('.game-title'));
+      browser.wait(EC.visibilityOf(team), 4000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
+      expect(teamTitle.getText()).toEqual('Test');
+      game.click();
+      browser.wait(EC.visibilityOf(gameTitle), 4000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+    });
+
+    it('Edit game button is not visible', () => {
+      var team = element.all(by.css('.team')).get(0);
+      var teamTitle = element(by.css('.team-title'));
+      var game = element.all(by.css('.game')).get(0);
+      var gameTitle = element(by.css('.game-title'));
+      var editGame = element(by.css('.edit-game'));
+      browser.wait(EC.visibilityOf(team), 4000);
+      team.click();
+      browser.wait(EC.visibilityOf(teamTitle), 4000);
+      expect(teamTitle.getText()).toEqual('Test');
+      game.click();
+      browser.wait(EC.visibilityOf(gameTitle), 4000);
+      expect(gameTitle.getText()).toEqual('Game Details');
+      expect(EC.invisibilityOf(editGame));
     });
   });
 });

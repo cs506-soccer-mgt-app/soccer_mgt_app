e7f4e4c73c9fd2d171538796ce1c233c80579368
Benjamin Nisler <bnisler@gmail.com>
2019-04-20 16:17:31 -0500

    add real service call

--- src/app/game/user-game-list/user-game-list.page.ts
+++ src/app/game/user-game-list/user-game-list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {CognitoService} from '../../services/cognito-service.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-user-game-list',
@@ -13,13 +14,26 @@ export class UserGameListPage implements OnInit {
   public userGameList = [];
 
   constructor(public userService: UserService,
-              public cognitoService: CognitoService) { }
+              public cognitoService: CognitoService,
+              public toastCtrl: ToastController) { }
 
   ngOnInit() {
     if (this.cognitoService.isLoggedIn()) {
       this.user = this.cognitoService.getUser();
-      // TODO: change to subscription
-      this.userGameList = this.userService.getGamesForUser(this.user.idToken.payload['cognito:username']);
+      this.userService.getGamesForUser(this.user.idToken.payload['cognito:username'])
+          .subscribe(res => {
+            for (const game of Object.values(res)) {
+              this.userGameList.push(game);
+            }
+          }, async err => {
+            const toast = await this.toastCtrl.create({
+              message: err.message,
+              duration: 3000,
+              position: 'bottom',
+              color: 'danger'
+            });
+            toast.present();
+          });
     }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,15 +13,8 @@ export class UserService {
     return this.http.get(`https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users/${id}/teams`);
   }
 
-  // TODO: REPLACE WITH REAL HTTP CALL
   getGamesForUser(id: string) {
-    // return this.http.get(`https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users/${id}/games`);
-    return [
-      { user_id: '3b4e98ee-6c71-496d-b801-6014fdf8d698', player_id: 9, payment: '0', team_id: 6, name: 'GOAT', primary_color: 'Gold', alternate_color: 'Black', manager_id: '24666164-bfec-4dd3-8a41-3f777d3d23c4', session_id: 4, game_id: 2, date: '2019-06-18', time: '19:48:16', opponent: 'WOAT', score: null, location: 'Home' },
-      { user_id: '3b4e98ee-6c71-496d-b801-6014fdf8d698', player_id: 9, payment: '0', team_id: 6, name: 'GOAT', primary_color: 'Gold', alternate_color: 'Black', manager_id: '24666164-bfec-4dd3-8a41-3f777d3d23c4', session_id: 4, game_id: 3, date: '2019-08-19', time: '21:49:04', opponent: 'Summer', score: null, location: 'Hawaii' },
-      { user_id: '3b4e98ee-6c71-496d-b801-6014fdf8d698', player_id: 9, payment: '0', team_id: 6, name: 'GOAT', primary_color: 'Gold', alternate_color: 'Black', manager_id: '24666164-bfec-4dd3-8a41-3f777d3d23c4', session_id: 4, game_id: 4, date: '2020-12-13', time: '00:40:13', opponent: 'Test', score: null, location: 'Practice Field' },
-      { user_id: '3b4e98ee-6c71-496d-b801-6014fdf8d698', player_id: 9, payment: '0', team_id: 6, name: 'GOAT', primary_color: 'Gold', alternate_color: 'Black', manager_id: '24666164-bfec-4dd3-8a41-3f777d3d23c4', session_id: 4, game_id: 5, date: '2021-02-17', time: '09:59:50', opponent: 'WOAT', score: null, location: 'Barcelona' }
-    ];
+    return this.http.get(`https://1d59ipr7q8.execute-api.us-east-2.amazonaws.com/production/users/${id}/games`);
   }
 
 }
